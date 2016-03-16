283d3ef0f5989508db730d87ce0f0134b70a6de5
shreyaskulur <shreyaskulur@gmail.com>
2016-03-16 23:56:09 +0530

    added program to get more deatils

--- VTU results details.py
+++ VTU results details.py
@@ -3,6 +3,8 @@ import requests
 usn_prefix = "4so15ee"
 last_usn = 50
 no_of_subjects = 8
+SGPA_list = []
+name_list = []
 for num in range(1,last_usn + 1):
     if num < 10 :
     	usn = usn_prefix + "00" + str(num)
@@ -19,7 +21,14 @@ for num in range(1,last_usn + 1):
     sgpa = soup.find(id="lblSGPA").b.string
 
     if student_name and sgpa:
+        name_list.append(student_name)
+        SGPA_list.append(sgpa)
         print(usn.upper()+" - "+student_name)
         print("      SGPA = " + sgpa)
+        for sub in range(1,no_of_subjects) :
+            grade_points = soup.find(id="txtGP"+ str(sub)).get('value')
+            grade_letter = soup.find(id="txtGardeLetter"+ str(sub)).get('value')
+            subject = soup.find(id="txtSub"+ str(sub)).get('value')
+            print(grade_letter + "/" + grade_points +" -> "+ subject)
         print("\n")
 
